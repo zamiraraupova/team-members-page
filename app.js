284569c9e99563fd69badca2cf3a4f3147a470f8
zamiraraupova <87194673+zamiraraupova@users.noreply.github.com>
2022-03-12 10:02:32 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+
+// let switchTheme = document.querySelector('.switch')
+// console.log(switchTheme)
+
+// function toggle(){
+    
+// }
+
+function toggleDarkMode(){
+    let element = document.body;
+    //console.log(element)
+    element.classList.toggle('dark-mode')
+}
+
+toggleDarkMode()
